e7eeeccea276bf338cf0ba8413be3013ecd91f40
Herman Gouw <hermangouw@gmail.com>
2020-03-18 20:33:14 +1100

    Added the project Logging.Test

---
+++ Logging.Test/LogTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logging;
+
+namespace Logging.Test
+{
+    [TestClass]
+    public class LogTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
