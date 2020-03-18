ffd077ea3d78cc9e0c1925d677940c0d60e10fa2
Herman Gouw <hermangouw@gmail.com>
2020-03-18 20:24:13 +1100

    Added the class Log

---
+++ Logging/Log.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+
+namespace Logging
+{
+    public class Log
+    {
+        public IAmazonS3 S3Client { get; set; }
+        public TransferUtility TransferUtility { get; set; }
+        public string BucketName { get; set; }
+        public string AccessKeyId { get; set; }
+        public string SecretAccessKey { get; set; }
+        public RegionEndpoint Region { get; set; }
+
+        public Log()
+        {
+        }
+
+        public Log(string bucketName)
+        {
+            BucketName = bucketName;
+        }
+
+        public Log(string bucketName, string accessKeyId, string secretAccessKey, RegionEndpoint region)
+        {
+            BucketName = bucketName;
+            AccessKeyId = accessKeyId;
+            SecretAccessKey = secretAccessKey;
+            Region = region;
+        }
+    }
+}
