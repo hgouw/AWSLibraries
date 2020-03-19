059f4781f809f283642619b2ecbab20ef75f3ae5
Herman Gouw <hermangouw@gmail.com>
2020-03-19 13:11:18 +1100

    More work

--- Logging/Log.cs
+++ Logging/Log.cs
@@ -9,12 +9,11 @@ namespace Logging
 {
     public class Log
     {
-        public IAmazonS3 S3Client { get; set; }
-        public TransferUtility TransferUtility { get; set; }
         public string BucketName { get; set; }
         public string AccessKeyId { get; set; }
         public string SecretAccessKey { get; set; }
         public RegionEndpoint Region { get; set; }
+        public TransferUtility TransferUtility { get; set; }
 
         public Log()
         {
@@ -31,6 +30,21 @@ namespace Logging
             AccessKeyId = accessKeyId;
             SecretAccessKey = secretAccessKey;
             Region = region;
+            TransferUtility = new TransferUtility(accessKeyId, secretAccessKey, region);
+        }
+
+        public void Write(string message, string category, string bucketName = null)
+        {
+
+        }
+
+        public void WriteLine()
+        {
+            //var jsons = new List<Json>();
+            //jsons.Add(new Json("Kaori", "4"));
+            //jsons.Add(new Json("Mimi", "1"));
+            //var serializer = new DataContractJsonSerializer(typeof(List<Json>));
+            //serializer.WriteObject(stream, jsons);
         }
     }
 }
